020d6b6081e93f2401a7a9ba98b7bc1943f96fbc
Jared <88jarmil@gmail.com>
2023-01-19 03:14:36 -0600

    first

---
+++ script.js
@@ -0,0 +1,33 @@
+const pA = document.getElementById('pA')
+const pB = document.getElementById('pB')
+const pC = document.getElementById('pC')
+const button = document.getElementById('toggle')
+const pElements = [pA, pB, pC]
+
+const displayOn = (value) => {
+value.style.display = 'block';
+}
+
+const displayOff = (value) => {
+    value.style.display = 'none';
+}
+
+const removeElement = (array) => {
+    array.shift()
+}
+
+
+
+const checkElements = (value) => {
+    if (value.length === 0) {
+displayOff(pA);
+displayOff(pB);
+displayOff(pC);
+    }
+} 
+
+button.addEventListener('click', () => {
+    displayOn(pElements[0]);
+    removeElement(pElements);
+    checkElements(pElements);
+})
